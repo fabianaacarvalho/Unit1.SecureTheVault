7523a5a0d01f9984d45803916557817f75f6f1c1
Fabiana Carvalho <fabiana.acarvalho@outlook.com>
2025-06-11 14:21:07 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 50 - 20 / 2;
+let code3 = (28 % 5) + 36;
+let message = "The vault has been secured. The combination is:";
+let codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message);
+console.log(codeA);
+console.log(codeB);
